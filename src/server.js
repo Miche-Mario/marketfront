963197e231194bbe9079105c8a2affafb59eaf2c
Miche Mario <Gmespace.74@gmail.com>
2024-04-05 13:07:30 +0000

    socket endpoint

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 //export const server = "http://localhost:8000/api/v2";
 
-export const server = "https://marketplacebackend-df3e.onrender.com/api/v2";
+export const server =
+  "https://marketplacebackend-df3e.onrender.com:8000/api/v2";
 //export const backend_url = "http://localhost:8000/";
 
 export const backend_url = "https://marketplacebackend-df3e.onrender.com:8000/";
