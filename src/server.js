86ba98d6eb430a4d25cd33aa1e37bb32292865d2
Miche Mario <Gmespace.74@gmail.com>
2024-04-05 12:23:06 +0000

    commit

---
+++ src/server.js
@@ -0,0 +1,6 @@
+//export const server = "http://localhost:8000/api/v2";
+
+export const server = "https://marketplacebackend-df3e.onrender.com/api/v2";
+//export const backend_url = "http://localhost:8000/";
+
+export const backend_url = "https://marketplacebackend-df3e.onrender.com:8000/";
